a6e063b72ec5cf5b9abe3065363dfec877cb7c58
E2sachu <amion.ange@gmail.com>
2014-04-14 01:45:36 +0200

    Travail sur ChoixMenu.java
    - 3/3 Spinners remplis
    - Rajout de deux TextViews
    - Test récupération de la valeur d'un spinner (fait sur array_cereal)

--- gen/com/essachu/emc/R.java
+++ gen/com/essachu/emc/R.java
@@ -9,6 +9,9 @@ package com.essachu.emc;
 
 public final class R {
     public static final class array {
+        public static final int array_calories=0x7f060003;
+        public static final int array_cereal=0x7f060002;
+        public static final int array_fruit=0x7f060001;
         public static final int array_lait=0x7f060000;
     }
     public static final class attr {
@@ -28,18 +31,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
+        public static final int action_settings=0x7f09000d;
         public static final int breakfast=0x7f090001;
         public static final int cereal=0x7f090002;
         public static final int container=0x7f090000;
         public static final int fruit=0x7f090006;
         public static final int lait=0x7f090003;
-        public static final int menu_alea=0x7f090009;
-        public static final int menu_choice=0x7f09000a;
-        public static final int next=0x7f090008;
+        public static final int menu_alea=0x7f09000b;
+        public static final int menu_choice=0x7f09000c;
+        public static final int next=0x7f09000a;
         public static final int spinner_cereal=0x7f090004;
         public static final int spinner_fruit=0x7f090007;
         public static final int spinner_lait=0x7f090005;
+        public static final int textView2=0x7f090008;
+        public static final int total=0x7f090009;
     }
     public static final class layout {
         public static final int activity_choix_alea=0x7f030000;
@@ -65,6 +70,7 @@ public final class R {
         public static final int next=0x7f05000c;
         public static final int title_activity_choix_alea=0x7f050006;
         public static final int title_activity_choix_menu=0x7f050005;
+        public static final int total=0x7f05000d;
     }
     public static final class style {
         /** 
--- src/com/essachu/emc/ChoixMenu.java
+++ src/com/essachu/emc/ChoixMenu.java
@@ -2,28 +2,44 @@ package com.essachu.emc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class ChoixMenu extends Activity implements OnItemClickListener {
+public class ChoixMenu extends Activity implements OnItemClickListener, OnItemSelectedListener {
 	
-	Spinner spin_lait;
+	Spinner spin_lait,spin_fruit,spin_cereal;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choix_menu);
 		
-		spin_lait = (Spinner) findViewById(R.id.spinner_lait);
+		spin_lait = (Spinner)findViewById(R.id.spinner_lait);
+		spin_fruit =(Spinner)findViewById(R.id.spinner_fruit);
+		spin_cereal = (Spinner)findViewById(R.id.spinner_cereal);
 		
 		ArrayAdapter<CharSequence> adapter_lait = ArrayAdapter.createFromResource(this, R.array.array_lait, android.R.layout.simple_spinner_item);
 		adapter_lait.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spin_lait.setAdapter(adapter_lait);
+		
+		ArrayAdapter<CharSequence> adapter_fruit = ArrayAdapter.createFromResource(this, R.array.array_fruit, android.R.layout.simple_spinner_item);
+		adapter_lait.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin_fruit.setAdapter(adapter_fruit);
+		
+		ArrayAdapter<CharSequence> adapter_cereal = ArrayAdapter.createFromResource(this, R.array.array_cereal, android.R.layout.simple_spinner_item);
+		adapter_lait.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin_cereal.setAdapter(adapter_cereal);
+		
+		spin_lait.setOnItemSelectedListener(this);
+		spin_fruit.setOnItemSelectedListener(this);
+		spin_cereal.setOnItemSelectedListener(this);
 		
 
 	}
@@ -50,6 +66,22 @@ public class ChoixMenu extends Activity implements OnItemClickListener {
 
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		
+		//on récupère la valeur numérique du spinner
+		int cereal_int = spin_cereal.getSelectedItemPosition();
+		Log.i("spin_cereal",""+cereal_int);
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
 		
 	}
